81edf3fb9495641e8ff156cf40f81c8275936b5b
Jp <jpedro.bergamasco@gmail.com>
2025-12-08 15:43:02 -0300

    commit2

---
+++ 05-structs-and-customtypes/14-practice-project/03-Adding a Method/note/note.go
@@ -0,0 +1,24 @@
+package note
+
+import (
+	"errors"
+	"time"
+)
+
+type Note struct {
+	title    string
+	content  string
+	createAt time.Time
+}
+
+func New(title, content string) (Note, error) {
+	if title == "" || content == "" {
+		return Note{}, errors.New("invalid input")
+	}
+
+	return Note{
+		title:    title,
+		content:  content,
+		createAt: time.Now(),
+	}, nil
+}
