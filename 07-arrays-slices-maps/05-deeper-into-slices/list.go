1e17f087ea656cc1fa3a7b367234c05c619a27a9
JpedroBHZ <jpedro.bergamasco@gmail.com>
2025-12-10 21:45:01 -0300

    commit6

---
+++ 07-arrays-slices-maps/05-deeper-into-slices/list.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var productNames [4]string = [4]string{"A book"}
+	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	fmt.Println(prices)
+
+	productNames[2] = "A Carpet"
+
+	fmt.Println(productNames)
+
+	fmt.Println(prices[2])
+
+	featurePrices := prices[1:] //cortando até o primeiro elemento a esquerda, perdeu
+	featurePrices[0] = 199.99
+	highlightedPrices := featurePrices[:1] //cortando até o 1 elemento a direita, ocultando o resto
+	fmt.Println(highlightedPrices)
+	fmt.Println(prices)
+	fmt.Println(len(highlightedPrices), cap(highlightedPrices)) //qtd elementos x qtd total
+
+	highlightedPrices = highlightedPrices[:3] //quando cortamos a direita, ficou salvo na memoria do go
+	fmt.Println(highlightedPrices)            //por cortarmos a esquerda ele se perde
+	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+}
