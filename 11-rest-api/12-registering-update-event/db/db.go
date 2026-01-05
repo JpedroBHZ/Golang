2256af19d913126f6dd86b7260955fbd632e5491
Jp <jpedro.bergamasco@gmail.com>
2026-01-05 17:25:02 -0300

    commit13

---
+++ 11-rest-api/12-registering-update-event/db/db.go
@@ -0,0 +1,49 @@
+package db
+
+//go get github.com/mattn/go-sqlite3
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3" //esse import nunca será usado mas precisamos dele, o "_" é para não sumir ao salvar
+)
+
+// Instancia variavel global para o banco
+var DB *sql.DB
+
+func InitDB() {
+
+	var err error
+
+	DB, err = sql.Open("sqlite3", "api.db") //Cria a conexão com o banco
+
+	if err != nil {
+		panic("Could not connect to database")
+	}
+
+	//Ali em cima, não criamos o banco exatamente, apenas abrimos ele para conexões, aqui configuramos a quantidade delas
+	DB.SetMaxOpenConns(10) //Maximo de requisições
+	DB.SetMaxIdleConns(5)  //Sempre ficaram 5 espaços, esperando conexões
+
+	createTables() //Função de criar tabelas
+}
+
+// Criando tabelas
+// Toda vez que iniciarmos é executado esse codigo que testa se a tabela existe, se não ele cria ela
+func createTables() {
+	createEventsTable := `
+	CREATE TABLE IF NOT EXISTS events ( 
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL, 
+		description TEXT NOT NULL, 
+		location TEXT NOT NULL, 
+		dateTime DATETIME NOT NULL, 
+		user_id INTEGER
+	)
+	`
+	_, err := DB.Exec(createEventsTable) //Cria os eventos
+
+	if err != nil {
+		panic("Could not create events table" + err.Error())
+	}
+
+}
---
+++ 11-rest-api/12-registering-update-event/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(server *gin.Engine) {
+
+	//Configurando manipulador de solicitação http get (GET, POST, PUT, PATCH, DELETE)
+	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent) //rota do evento por id
+	server.POST("/events", createEvents)
+	server.PUT("/events/:id")
+
+}
